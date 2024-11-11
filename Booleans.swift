b0d4404d616769c8d2ef426bf26fbd436fd7b31b
L <60981745+pankourgiou@users.noreply.github.com>
2024-11-11 06:58:37 +0200

    Create Booleans.swift

---
+++ Booleans.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+let  ozzottoIsCool = true // Boolean variable
+let  ozzottoIsNotCool = false // Another Boolean variable
+
+// Simple conditional check
+if ozzottoIsCool {
+    print("He is cool indeed")
+} else {
+    print("He is cool really")
+}
